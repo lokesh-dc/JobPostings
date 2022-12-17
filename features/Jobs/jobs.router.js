2b1172e8127bf689706c908ec4e5f8febdde99ca
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-12-17 15:14:47 +0530

    Filtering based on Roles

--- features/Jobs/jobs.router.js
+++ features/Jobs/jobs.router.js
@@ -5,13 +5,18 @@ const app = express.Router();
 const JobsModel = require("./jobs.schema");
 
 app.get("/", async(req, res)=>{
-    const { sort, language, page } = req.query;
+    const { sort, language, page, role } = req.query;
     let jobs;
     try{
-        if(language===undefined || language==="")
+        if((language===undefined || language==="") && (role===undefined || role===""))
             jobs = await JobsModel.find().limit(10).skip(page*10).sort({postedAt : sort});
         else
-            jobs = await JobsModel.find({language}).limit(10).skip(page*10).sort({postedAt : sort});
+            if(language!=="" && role!=="")
+                jobs = await JobsModel.find({language,role}).limit(10).skip(page*10).sort({postedAt : sort});
+            else if(role!=="")
+                jobs = await JobsModel.find({role}).limit(10).skip(page*10).sort({postedAt : sort});
+            else
+                jobs = await JobsModel.find({language}).limit(10).skip(page*10).sort({postedAt : sort});
         res.send(jobs);
     }catch(e){
         res.status(500).send({status: false, message:"Something went wrong"});
